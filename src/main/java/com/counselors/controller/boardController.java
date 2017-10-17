baf96f23e29d85549a9cc087f71c2f87661e0834
Mr.choi <zl0759vk@naver.com>
2017-10-17 17:34:02 +0900

    1

--- src/main/java/com/counselors/controller/boardController.java
+++ src/main/java/com/counselors/controller/boardController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.boot.domain.BoardVO;
 import com.counselors.domain.CounselorsVO;
 import com.counselors.mapper.CounselorsMapper;
 
@@ -20,41 +21,55 @@ public class boardController {
 	private CounselorsMapper conMapper;
 
 	// List 글목록
-	@RequestMapping(value="/counselors/",method = RequestMethod.GET)
+	@RequestMapping(value = "/counselors/", method = RequestMethod.GET)
 	public ModelAndView counselorList() throws Exception {
 		List<CounselorsVO> list = conMapper.counselorList();
-		return new ModelAndView("counselorsList","list",list);
+		return new ModelAndView("counselorsList", "list", list);
 
 	}
-	
+
 	// 글 생성 페이지
-	@RequestMapping(value="/counselors", method = RequestMethod.GET)
-	public ModelAndView counselorWrite() throws Exception{	
+	@RequestMapping(value = "/counselors", method = RequestMethod.GET)
+	public ModelAndView counselorWrite() throws Exception {
 		return new ModelAndView("counselorsWriter");
 	}
-	
-	//글 작성
-	@RequestMapping(value="/counselors", method = RequestMethod.POST)
-	public String write(@ModelAttribute("CounselorsVO")CounselorsVO con) throws Exception{
+
+	// 글 작성
+	@RequestMapping(value = "/counselors", method = RequestMethod.POST)
+	public String write(@ModelAttribute("CounselorsVO") CounselorsVO con) throws Exception {
 		conMapper.counselorInsert(con);
 		return "redirect://localhost:8080/counselors/";
-		
+
 	}
-	
-	//글 상세 정보
-	@RequestMapping(value="/counselors/{bno}",method = RequestMethod.GET)
-	public ModelAndView counselorView(@PathVariable("bno") int bno) throws Exception{
+
+	// 글 상세 정보
+	@RequestMapping(value = "/counselors/{bno}", method = RequestMethod.GET)
+	public ModelAndView counselorView(@PathVariable("bno") int bno) throws Exception {
 		CounselorsVO counselorVO = conMapper.counselorView(bno);
-		return new ModelAndView("counselorsView","counselorVO",counselorVO);
+		return new ModelAndView("counselorsView", "counselorVO", counselorVO);
 	}
-	
-	//글 삭제
-	@RequestMapping(value="/counselors/{bno}",method = RequestMethod.DELETE)
-	public String counselorDelete(@PathVariable("bno") int bno) throws Exception{
+
+	// 글 삭제
+	@RequestMapping(value = "/counselors/{bno}", method = RequestMethod.DELETE)
+	public String counselorDelete(@PathVariable("bno") int bno) throws Exception {
 		conMapper.counselorDelete(bno);
 		return "redirect://localhost:8080/counselors/";
 	}
-	
-	//글 수정
- 
+
+	// 글 수정 get
+	// 글 상세 정보
+	@RequestMapping(value = "/counselors/{bno}", method = RequestMethod.GET)
+	public ModelAndView counselorUpdate(@PathVariable("bno") int bno) throws Exception {
+		CounselorsVO counselorVO = conMapper.counselorView(bno);
+		return new ModelAndView("counselorsUpdate", "counselorVO", counselorVO);
+	}
+
+	// 글 수정put
+	@RequestMapping(value = "/counselors/{bno}", method = RequestMethod.PUT)
+	public String Update(@ModelAttribute("CounselorsVO") CounselorsVO conVo, @PathVariable("bno") int bno)
+			throws Exception {
+		conMapper.counselorUpdate(conVo);
+		return "redirect://localhost:8080/counselors/";
+	}
+
 }
--- src/main/webapp/WEB-INF/views/counselorsUpdate.jsp
+++ src/main/webapp/WEB-INF/views/counselorsUpdate.jsp
@@ -4,7 +4,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=EUC-KR">
-<title>Insert title here</title>
+<title>게시물 수정</title>
 </head>
 <body>
 
